a1975e44021eec2e1dd633e9feb486adc1f784b2
Ysidro <39842830+Ysidr@users.noreply.github.com>
2024-12-02 15:35:21 +0100

    Design Summary

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const BASE_URL = "https://join-7cb80-default-rtdb.europe-west1.firebasedatabase.app/";
 
 let UsersAmountViaId = 0;
+let currentTime = new Date()
+
+
 
 let toDoTaskCount = 0;
 let awaitFeedbackTaskCount = 0;
--- scripts/summary.js
+++ scripts/summary.js
@@ -1,4 +1,7 @@
+
 async function initSummary() {
+    console.log(currentTime);
+    
     await updateAllTaskCounts();
     displayCounts();
 }
@@ -22,36 +25,36 @@ function displayCounts() {
 
 async function getUrgentAmount() {
     urgentAmount = 0;
-            await checkToDo()
-            await checkAwait()
-            await checkInProgress() 
+    await checkToDo()
+    await checkAwait()
+    await checkInProgress()
 }
 
 async function checkToDo() {
     let response = await fetch(BASE_URL + `Tasks/ToDo.json`);
     responseToJson = await response.json();
-        for (let indexIterate1Task = 1; indexIterate1Task < responseToJson.length; indexIterate1Task++) {
-            if (responseToJson[indexIterate1Task].priority == "high") {
-                urgentAmount++
-            }
+    for (let indexIterate1Task = 1; indexIterate1Task < responseToJson.length; indexIterate1Task++) {
+        if (responseToJson[indexIterate1Task].priority == "high") {
+            urgentAmount++
         }
+    }
 
 }
 async function checkAwait() {
     let response = await fetch(BASE_URL + `Tasks/AwaitFeedback.json`);
     responseToJson = await response.json();
-        for (let indexIterate1Task = 1; indexIterate1Task < responseToJson.length; indexIterate1Task++) {
-            if (responseToJson[indexIterate1Task].priority == "high") {
-                urgentAmount++
-            }
+    for (let indexIterate1Task = 1; indexIterate1Task < responseToJson.length; indexIterate1Task++) {
+        if (responseToJson[indexIterate1Task].priority == "high") {
+            urgentAmount++
         }
+    }
 }
 async function checkInProgress() {
     let response = await fetch(BASE_URL + `Tasks/InProgress.json`);
     responseToJson = await response.json();
-        for (let indexIterate1Task = 1; indexIterate1Task < responseToJson.length; indexIterate1Task++) {
-            if (responseToJson[indexIterate1Task].priority == "high") {
-                urgentAmount++
-            }
+    for (let indexIterate1Task = 1; indexIterate1Task < responseToJson.length; indexIterate1Task++) {
+        if (responseToJson[indexIterate1Task].priority == "high") {
+            urgentAmount++
         }
+    }
 }
