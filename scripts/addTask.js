5f6cef2ac6a9486f99bdf2632ff83323826089f5
Ysidro <39842830+Ysidr@users.noreply.github.com>
2024-12-15 15:52:05 +0100

    Whyyyy? I dont get why its noch selected anymore

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -136,6 +136,8 @@ function contactSelected(name, bgColor) {
         console.log(selectedContatct);
     }
     document.getElementById(name).classList.toggle("selectedContact")
+    document.getElementById(name).selected = true
+
     getInitialsOfAddedUser()
 }
 
