3b63cc83008aeaaca6c5ceb18892a3ede63d5f9a
Alexander-Bartmann <seirex.mb@gmail.com>
2025-02-21 15:11:31 +0100

    Ändere die BASE_URL für die Firebase-Datenbank

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://join-7cb80-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL = "https://joinclone-default-rtdb.europe-west1.firebasedatabase.app/";
 let UsersAmountViaId = 0;
 let currentTime = new Date()
 let currentDraggedId;
